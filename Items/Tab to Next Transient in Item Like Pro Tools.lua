1c20da8aba3977a250658f718786839894cf9d4b
schapps <stephenschappler@gmail.com>
2024-08-24 12:15:01 -0700

    fixing bug in tab to transient

--- Items/Tab to Next Transient in Item Like Pro Tools.lua
+++ Items/Tab to Next Transient in Item Like Pro Tools.lua
@@ -1,11 +1,12 @@
 -- @description Tab to Next Transient in Item, Like Pro Tools
 -- @author Stephen Schappler
--- @version 1.0
+-- @version 1.1
 -- @about
 --   A script to emulate the tab to transient behavior in Pro Tools
 -- @link https://www.stephenschappler.com
 -- @changelog 
---   08/23/24 Creating the script
+--   08/23/24 v1.0 - Creating the script
+--   08/24/14 v1.1 - Fixing a bug where the cursor would get stuck if there was no time between two items
 
 -- Function to tab to the next transient within the selected item
 function tab_to_transient_in_item()
@@ -24,6 +25,15 @@ function main()
         -- If the cursor is at or past the end of the item, move to the next item
         if cur_pos >= item_end then
             reaper.SetMediaItemSelected(item, false) -- Deselect current item
+            
+            --Gotta move the cursor back a little because of how action 40417 behaves
+            -- Get the current edit cursor position
+            local cur_pos = reaper.GetCursorPosition()
+            -- Move the edit cursor back by 1 ms (0.001 seconds)
+            local new_pos = cur_pos - 0.0001
+            -- Set the new cursor position
+            reaper.SetEditCurPos(new_pos, true, false)
+    
             reaper.Main_OnCommand(40417, 0) -- Move to next item and select it
         else
             tab_to_transient_in_item()
