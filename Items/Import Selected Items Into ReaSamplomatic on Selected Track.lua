9ad69f95a5cd581a7e86c655e2f97291c8ed0631
schapps <stephenschappler@gmail.com>
2024-09-04 17:00:46 -0700

    updating reasamplomatic script, still not done

--- Items/Import Selected Items Into ReaSamplomatic on Selected Track.lua
+++ Items/Import Selected Items Into ReaSamplomatic on Selected Track.lua
@@ -3,7 +3,7 @@
 -- @version 1.0
 -- Initial Script by MPL Modified by Analogmad (Chris Kowalski), and further modified by Stephen Schappler
 -- From Analogmad: MPL Helped me with this via the Reaper Forums. I had an issue of putting all samples into one RS5K. Added Arming for Audio Recording. Added Velocity Randomizer
--- From Stephen Schappler: I changed the MIDI Velcotiy randomizer this script uses (it now uses one I wrote for this purpose), as well as the actions used to render items to a new takes before and after getting loaded into the sampler
+-- From Stephen Schappler: I changed the MIDI Velcotiy randomizer this script uses (it now uses one I wrote for this purpose), as well as the actions used to render items to a new takes before and after getting loaded into the sampler                   
 -- @about
 --   Import Selected Items Into ReaSamplomatic on Selected Track
 -- @link https://www.stephenschappler.com
@@ -13,12 +13,17 @@
 local script_title = 'Import Selected Items into ReaSamplomatic on Selected Track'
 -------------------------------------------------------------------------------    
 function GetRS5kID(tr)
-local id = -1
-for i = 1, reaper.TrackFX_GetCount(tr) do
-  if ({reaper.TrackFX_GetFXName(tr, i-1, '')})[2]:find('RS5K') then return i-1 end
+    local id = -1
+    for i = 1, reaper.TrackFX_GetCount(tr) do
+        local retval, fx_name = reaper.TrackFX_GetFXName(tr, i-1, '')
+        -- Check for either "RS5K" or "ReaSamplOmatic5000" in the FX name
+        if fx_name:find('RS5K') or fx_name:find('ReaSamplOmatic5000') then
+            return i - 1 -- Return the index of the RS5K instance
+        end
+    end
+    return id -- Return -1 if no RS5K or ReaSamplomatic is found
 end
-return id
-end
+
 ------------------------------------------------------------------------------- 
 function GetMidiToolID(tr)
 local id = -1
@@ -27,6 +32,21 @@ for i = 1, reaper.TrackFX_GetCount(tr) do
   if fxName:find('Randomize') then return i-1 end
 end
 return id
+end
+-------------------------------------------------------------------------------
+function ClearExistingSamples(track, rs5k_pos)
+    -- Check if a valid ReaSamplomatic instance exists
+    if rs5k_pos == -1 then
+        reaper.ShowMessageBox("No ReaSamplomatic found on the selected track!", "Error", 0)
+        return
+    end
+    
+    -- Clear all sample slots (FILE0 to FILE127) in the ReaSamplomatic instance
+    for i = 0, 127 do
+        reaper.TrackFX_SetNamedConfigParm(track, rs5k_pos, "FILE" .. i, "")
+    end
+    
+    --reaper.ShowMessageBox("Cleared all samples in ReaSamplomatic!", "Success", 0)
 end
 ------------------------------------------------------------------------------- 
 function ExportSelItemsToRs5k(track)   
@@ -49,6 +69,9 @@ if rs5k_pos == -1 then
   rs5k_pos = reaper.TrackFX_AddByName(track, 'ReaSamplOmatic5000 (Cockos)', false, -1)
 end
 
+-- Clear the samples in ReaSamplomatic
+--ClearExistingSamples(track, rs5k_pos)
+
 -- Iterate through samples and add them to the same ReaSamplOmatic5000 instance
 for i = 1, number_of_samples do
   local item = reaper.GetSelectedMediaItem(0, i-1)
@@ -68,7 +91,6 @@ for i = 1, number_of_samples do
 end
 if rs5k_pos then reaper.TrackFX_SetNamedConfigParm(track, rs5k_pos, "DONE","") end
 end
-
 -------------------------------------------------------------------------------  
 function main(track)   
 -- track check
