beb71931710ad5bae35f02b338dd54601cddd83c
schapps <stephenschappler@gmail.com>
2024-10-31 11:57:56 -0700

    trying yet again to get the theme recognized

---
+++ Themes/ReArk_PlayStationStudios_Sound.lua
@@ -0,0 +1,8 @@
+--[[
+Description: ReARK PlayStation Studios Sound
+Version: 1.0
+Author: Stephen Schappler
+Provides:
+    [theme] ReARK_PlayStationStudios_Sound.ReaperThemeZip
+--]]
+reaper.OpenColorThemeFile 'ReARK_PlayStationStudios_Sound.ReaperThemeZip'
